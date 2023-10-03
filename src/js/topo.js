ecb06b92a63c2fd34f19224a91a7a86794b2422d
Enne <EnneFofe10@gmail.com>
2023-10-03 16:16:04 -0300

    Inclusividade
    Deixando o site mais inclusivo que antes e adicionando a opção no rodapé de voltar ao topo da página.

---
+++ src/js/topo.js
@@ -0,0 +1,7 @@
+// Chamando a tag que representa o botão de ir pro topo
+const topo = document.getElementById('topo')
+
+// Adicionando os eventos
+topo.addEventListener("click", function(){
+    window.scrollTo(0, 0)
+})
